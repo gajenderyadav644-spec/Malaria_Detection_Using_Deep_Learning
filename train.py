d3d51cfa39841cbad628a55657fb0bffcfe3d74b
Gajender Kumar <gajenderyadav644@gmail.com>
2026-03-05 16:46:51 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+data_dir = "cell_images"
+
+img_size = 128
+batch_size = 32
+epochs = 15
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_size, img_size),
+    batch_size=batch_size
+)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_size, img_size),
+    batch_size=batch_size
+)
+
+print("Classes:", train_ds.class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+model = models.Sequential([
+
+    layers.Rescaling(1./255, input_shape=(128,128,3)),
+
+    layers.Conv2D(32,3,activation="relu"),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(64,3,activation="relu"),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(128,3,activation="relu"),
+    layers.MaxPooling2D(),
+
+    layers.Conv2D(256,3,activation="relu"),
+    layers.MaxPooling2D(),
+
+    layers.Flatten(),
+
+    layers.Dense(256,activation="relu"),
+    layers.Dropout(0.5),
+
+    layers.Dense(1,activation="sigmoid")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.summary()
+
+model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
+)
+
+model.save("malaria_model.h5")
+
+print("✅ Model trained and saved")
